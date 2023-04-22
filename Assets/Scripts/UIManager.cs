b2c3e22b8d1d71932362c7c8e0c7138cde3aa820
Stefan Ledolter <s.ledolter@gmail.com>
2023-04-22 10:58:31 +0200

    SL: added check if buying buidlings is possible

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -15,6 +15,7 @@ public class UIManager : MonoBehaviour
   public GameObject gameResourceDisplayPrefab;
 
   private Dictionary<string, TMP_Text> _resourceTexts;
+  private Dictionary<string, Button> _buildingButtons;
 
   private void Awake() {
     // create texts for each in-game resource (gold, wood, stone, ...)
@@ -29,6 +30,7 @@ public class UIManager : MonoBehaviour
     _buildingPlacer = GetComponent<BuildingPlacer>();
 
     // create buttons for each building type
+    _buildingButtons = new Dictionary<string, Button>();
     for(int i = 0; i < Globals.BUILDING_DATA.Length; i++) {
       GameObject button = GameObject.Instantiate(
         buildingButtonPrefab,
@@ -39,6 +41,10 @@ public class UIManager : MonoBehaviour
       button.transform.Find("Text").GetComponent<TMP_Text>().text = code;
       Button b = button.GetComponent<Button>();
       _AddBuildingButtonListener(b, i);
+      _buildingButtons[code] = b;
+      if (!Globals.BUILDING_DATA[i].CanBuy()) {
+        b.interactable = false;
+      }
     }
   }
 
@@ -54,5 +60,11 @@ public class UIManager : MonoBehaviour
 
   private void _AddBuildingButtonListener(Button b, int i) {
     b.onClick.AddListener(() => _buildingPlacer.SelectPlacedBuilding(i));
+  }
+
+  public void CheckBuildingButtons() {
+    foreach(BuildingData data in Globals.BUILDING_DATA) {
+      _buildingButtons[data.Code].interactable = data.CanBuy();
+    }
   }
 }
